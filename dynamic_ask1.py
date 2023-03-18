6b022ba96a091ec0d5f45752144d10cce47fd157
gsiatras <gsiatras@isc.tuc.gr>
2023-03-18 13:38:42 +0200

    3d array

--- dynamic_ask1.py
+++ dynamic_ask1.py
@@ -1,20 +1,30 @@
 import numpy as np
 
 class GraphAgent:
-    def __init__(self, stages, nodes, cost, total_nodes):
+    def __init__(self, c, stages, nodes,total_nodes):
         self.stages = stages
         self.nodes = nodes
-        self.cost = cost
         self.total_nodes = total_nodes
+        self.ci = c
 
-        self.c = [[np.zeros((total_nodes + 1,))] for j in range(total_nodes + 1)]
-        self.ccost = np.zeros((total_nodes + 1,))  # array that holds the cost from 1 node to another (each possition is a node)
-        self.d = np.zeros((total_nodes + 1,))
-        self.path = np.zeros((total_nodes + 1,))
+        # self.c = np.full((self.total_nodes, self.total_nodes),1000)
+        # self.ccost = np.zeros((self.total_nodes ,))  # array that holds the cost from 1 node to another (each possition is a node)
+        # self.d = np.zeros((self.total_nodes ,))
+        # self.path = np.zeros((self.total_nodes ,))
 
     # function to create the dynamic area
-    def dynamic_array(self):
-        for i in range(self.total_nodes):
+    # def dynamic_array(self):
+    #     k = 1
+    #     for i in range(self.total_nodes):
+    #         for j in self.cost[i]:
+    #             if self.cost[i][j] < 1000:
+    #                 self.c[i][k] = self.cost[i][j]
+    #             k += 1
+    #
+    #     for i in range(self.stages - 1):
+    #         for j in range(int(self.nodes[i])):
+    #             for k in range(int(self.nodes[i + 1])):
+    #             c += 1
 
 
 
@@ -32,33 +42,36 @@ class GraphAgent:
         for i in range(self.stages - 1):
             for j in range(int(self.nodes[i])):
                 for k in range(int(self.nodes[i + 1])):
-                    print('%d\t\t %d\t\t\t %d\t\t %d\t\t %d' % (i, j, i + 1, k, self.cost[c][k]))
-                c += 1
-
+                    print('%d\t\t %d\t\t\t %d\t\t %d\t\t %d' % (i, j, i + 1, k, self.ci[i][j][k]))
 
 def menu():
     stages = int(input('Stages: \n'))
     nodes = np.zeros((stages,))
-    cost = []
     nodes[0] = 1
     num_nodes = 1
+    c = []
+
     for i in range(1, stages):
         nodes[i] = int(input('Nodes for stage %d:' %i))
         num_nodes += nodes[i]
     for i in range(stages - 1):
+        cost = []
         for j in range(int(nodes[i])):
             node_cost = []
             for k in range(int(nodes[i+1])):
                 node_cost.append(int(input('Cost from Stage %d Node %d to Stage %d Node %d:' % (i, j, i + 1, k))))
             cost.append(node_cost)
-    return stages, nodes, cost, num_nodes
+        c.append(cost)
+    return c, stages, nodes, num_nodes
 
 
 
 if __name__ == '__main__':
-    stages, nodes, cost, num_nodes= menu()
-    agent = GraphAgent(stages, nodes, cost, num_nodes)
+    c, stages, nodes, num_nodes = menu()
+    agent = GraphAgent(c, stages, nodes, num_nodes)
     agent.print_graph()
+    print('==========================================================================')
+
 
 
 
