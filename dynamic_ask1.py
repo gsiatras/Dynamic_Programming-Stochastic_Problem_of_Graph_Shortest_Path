3c104b001109fa7b9891d4025f9f7586d58acb57
gsiatras <gsiatras@isc.tuc.gr>
2023-05-18 21:15:55 +0300

    .

--- dynamic_ask1.py
+++ dynamic_ask1.py
@@ -1,26 +1,33 @@
 import numpy as np
 
 class GraphAgent:
-    def __init__(self, c, stages, nodes):
+    def __init__(self, c, stages, nodes, slipp):
         self.stages = stages
         self.nodes = nodes
         self.ci = c
+        self.p = 1 - slipp
+        self.slipp = slipp
+        self.pflag = 0      # flag to know if we have more than 1 possible edge so we have slip prob
 
         self.d = [[0 for k in range(self.nodes[j])] for j in range(self.stages)]
-        self.cost = [[0 for k in range(self.nodes[j])] for j in range(self.stages)]
+        self.Vtrans = [[[0 for l in range(self.nodes[j + 1])] for k in range(self.nodes[j])] for j in
+                       range(self.stages - 1)]  # array to store the Value of moving from one node to another
+
+        self.Vnode = [[0 for k in range(self.nodes[j])] for j in
+                      range(self.stages)]  # array to store the value of each node
         self.path = [0 for j in range(self.stages)]
         # print(self.cost)
 
     def printer(self):
-        print('============================Graph===============================')
-        self.print_graph()
-        print('================================================================')
-        print('------------------Path and cost for every node------------------')
-        # print path and cost for every node
-        print('Current: \t\t\t To: \nStage:\tNode:\t\tStage:\tNodes:\tCost to end:')
-        for i in range(self.stages - 1):
-            for j in range(int(self.nodes[i])):
-                print(' %d\t\t %d\t\t\t %d\t\t %d\t\t %d' % (i, j, i + 1, self.d[i][j], self.cost[i][j]))
+        # print('============================Graph===============================')
+        # self.print_graph()
+        # print('================================================================')
+        # print('------------------Path and cost for every node------------------')
+        # # print path and cost for every node
+        # print('Current: \t\t\t To: \nStage:\tNode:\t\tStage:\tNodes:\tCost to end:')
+        # for i in range(self.stages - 1):
+        #     for j in range(int(self.nodes[i])):
+        #         print(' %d\t\t %d\t\t\t %d\t\t %d\t\t %d' % (i, j, i + 1, self.d[i][j], self.cost[i][j]))
         print('================================================================')
         print('------------------Shortest Path and cost to end-----------------')
         for k in range(self.stages):
@@ -28,27 +35,63 @@ class GraphAgent:
                 print('(%d, %d)' % (k, self.path[k]))
             else:
                 print('(%d, %d) =>'  % (k, self.path[k]))
-        print('Total cost: %d' % self.cost[0][0])
+        print('Total cost: %d' % self.Vnode[0][0])
+
+
+    def findpslip(self, stage, node):
+        i = 0
+        m = 1000
+        for k in range(int(self.nodes[stage + 1])):
+            if (c[stage][node][k] < 1000):
+                i += 1
+                if(c[stage][node][k] < m):
+                    m = c[stage][node][k]
+                    best = k
+
+        if i > 1:
+            self.pflag = 1
+        else:
+            self.pflag = 0
+
+        return self.slipp/i, best
+
 
     #  function to calculate sortest path
     def calculate(self):
         for i in range(self.stages - 2, -1, -1):
             for j in range(self.nodes[i]):
-                min = 1000
+                pslip, best = self.findpslip(i, j)
+                # calculate transition value for each node to the next
                 for k in range(int(self.nodes[i+1])):
+                    total_v = 0
                     # print('%d || %d || %d' %( i,j, k))
-                    if c[i][j][k] + self.cost[i+1][k] < min:
-                        min = c[i][j][k] + self.cost[i+1][k]
-                        self.d[i][j] = k
-                self.cost[i][j] = min
-        # find sortest path
+                    # if flag raised then we have multiple possible routes
+                    if self.pflag == 1:
+                        if k == best:
+                            self.Vtrans[i][j][k] = self.Vnode[i + 1][k] + c[i][j][k] * (1 - self.p)
+                            total_v += self.Vtrans[i][j][k]
+                        else:
+                            self.Vtrans[i][j][k] = self.Vnode[i + 1][k] + c[i][j][k] * (1 - pslip)
+                            total_v += self.Vtrans[i][j][k]
+                    else:
+                        self.Vtrans[i][j][k] = self.Vnode[i + 1][k] + c[i][j][k]
+                        total_v += self.Vtrans[i][j][k]
+                    # the value of the node
+                    self.Vnode[i][j] = total_v
+
+
+        # find best path
         self.path[0] = 0
-        for i in range(1, self.stages):
-            self.path[i] = self.d[i-1][self.path[i-1]]
+        for i in range(0, self.stages-1):
+            self.path[i+1] = np.argmin(self.Vtrans[i][self.path[i]][:])
         # print(self.d)
         # print results
         self.printer()
         # print(self.path)
+
+
+
+    # function to find slip probability per stage(depends on the number of possible steps)
 
     def print_graph(self):
         print('Current: \t\t\t To: \nStage:\t Node:\t\t Stage:\t Node:\t Cost:')
@@ -63,6 +106,7 @@ def menu():
     nodes = []
     nodes.append(1)
     c = []
+    p = -1
 
     for i in range(1, stages):
         nodes.append(int(input('Nodes for stage %d:' % i)))
@@ -74,17 +118,20 @@ def menu():
                 node_cost.append(int(input('Cost from Stage %d Node %d to Stage %d Node %d:' % (i, j, i + 1, k))))
             cost.append(node_cost)
         c.append(cost)
-    return c, stages, nodes
+    while p<0 or p>=1 :
+        slipp = float(input('Slip probability between[0,1): '))
 
+    return c, stages, nodes, slipp
 
 
 if __name__ == '__main__':
-    # c, stages, nodes= menu()
+    # c, stages, nodes, p= menu()
     # test graph1
     # c = [[[2, 3]], [[5, 1, 1000], [1000, 1000, 3]], [[6, 1000], [3, 2], [1000, 4]]]
     # stages = 4
     # nodes = [1, 2, 3, 2]
-    # ipnut graph
+    # input graph
+    slipp = 0.4
     stages = 7
     nodes = [1, 3, 3, 3, 4, 3, 2]
     c =[[[5, 7, 4]],
@@ -93,7 +140,7 @@ if __name__ == '__main__':
         [[4, 2, 1000, 6], [1000, 3, 1000, 1000], [5, 1000, 2, 4]],
         [[5, 1000, 4], [1000, 6, 1000], [1000, 3, 1000], [1000, 1000, 3]],
         [[7, 4], [8, 5], [6, 7]]]
-    agent = GraphAgent(c, stages, nodes)
+    agent = GraphAgent(c, stages, nodes, slipp)
     agent.calculate()
 
     while True:
